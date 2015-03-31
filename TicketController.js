a0e6dafedf9c9641eb5cc5348a40a604679381f8
AatuPitkanen <aatu.pitkanen@gmail.com>
2015-03-31 14:42:26 +0300

    Update

--- TicketController.js
+++ TicketController.js
@@ -3,7 +3,7 @@ var app = angular.module('MainApp', ['ngRoute', 'ui.bootstrap']);
 app.controller('TicketController', [ '$http', '$scope', function($http, $scope) {
      
 	    
-	    $scope.Lista = [];
+	        $scope.Lista = [];
 		$scope.GetQueue = [];
 		$scope.GetQueueTicket = [];
 		$scope.GetTicket = [];
@@ -11,24 +11,23 @@ app.controller('TicketController', [ '$http', '$scope', function($http, $scope) 
 		$scope.isCollapsed = false;
 		$scope.IdSelected = null;  
 		$scope.IdSelected2 = null;  
-	    $scope.RowClick = null;
+	
 		
 		 $http.get('http://127.0.0.1:8080/api/queue/').success(function(data) {
 		   $scope.GetQueue= data; 
 			}); 
  
- $scope.TicketCall = function (index, IdSelected, data, RowClick) {
+ $scope.TicketCall = function (IdSelected, data) {
    
     var data = data;
     $scope.IdSelected = IdSelected;
-	$scope.RowClick = RowClick;
-	
-	$http.get('http://127.0.0.1:8080/api/queue/').success(function(data2) {
-		$scope.GetQueue = data2;  
+ 
+	$http.get('http://127.0.0.1:8080/api/queue/').success(function(data) {
+		$scope.GetQueue = data;  
 	   console.log(IdSelected);
-		var url = data2[IdSelected].tickets;		
+		var url = data[IdSelected].tickets;		
 		
-		angular.forEach(url, function TicketCall(IdSelected) { //(var i = 0; i < arrayLength; ++i) //  for (i in url) {
+		angular.forEach(url, function TicketCall(IdSelected) { 
 		$http.get('http://127.0.0.1:8080'+ IdSelected).success(function (data) {
 		  $scope.GetQueueTicket.push(data);	
 		})
@@ -45,9 +44,9 @@ app.controller('TicketController', [ '$http', '$scope', function($http, $scope) 
 		var url2 = data[IdSelected2].articles;
 		
 		angular.forEach(url2, function ArticleCall(IdSelected2) {  
-		$http.get('http://127.0.0.1:8080'+ IdSelected2).success(function (data3) {
-         $scope.GetArticle = data3;	
-		  $scope.Lista.push(data3);
+		$http.get('http://127.0.0.1:8080'+ IdSelected2).success(function (data) {
+         $scope.GetArticle = data;	
+		  $scope.Lista.push(data);
 		})
 		$scope.Lista.length = 0;
 		 });
@@ -56,11 +55,10 @@ app.controller('TicketController', [ '$http', '$scope', function($http, $scope) 
 	
 	$scope.QueueCall = function() { 
    
-           $http.get('http://127.0.0.1:8080/api/queue/').success(function(data2) {
-		   $scope.GetQueue= data2;	 
+           $http.get('http://127.0.0.1:8080/api/queue/').success(function(data) {
+		   $scope.GetQueue= data;	 
 			}); 
 			}			
-	  		
     }]);
 	
 	
